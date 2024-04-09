b60c627676c54261534c004b291e94e8e001b19c
Cho Phan <callmechophan@gmail.com>
2024-04-09 22:59:08 +0700

    1.2.1 functions and stored procedures

---
+++ advanced_functions_and_stored_procedures/functionc_and_stored_procedures/main.sql
@@ -0,0 +1,9 @@
+/*
+ * functions
+ * - accept only input parameters
+ * - used to return a specific value
+ * 
+ * stored procedures
+ * - accept both input and output parameters
+ * - processing, manipulating and modify data
+*/
