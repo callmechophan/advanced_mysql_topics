cfc702d1fc8bbff0b6d6b51815c5e9bfc8edef93
Cho Phan <callmechophan@gmail.com>
2024-04-09 22:24:53 +0700

    1.1 introduction

---
+++ introduction/main.sql
@@ -0,0 +1,60 @@
+/*
+ * MySQL parameters
+ * - to create more complex stored procedure
+ * 
+ * User-Defined Functions (UDF)
+ * - how to develop UDF
+ * 
+ * MySQL triggers
+ * - to automate data based tasks
+ *
+ * Different type MySQL triggers
+ * - INSERT
+ * - UPDATE
+ * - DELETE
+ * 
+ * Scheduled event
+ * - how you can make use of scheduled events
+ * 
+ * CREATE EVENT [IF NOT EXISTS] event_name
+ * ON SCHEDULE AT CURRENT_TIMESTAMP [+ INTERVAL]
+ * DO
+ * event_body
+ * 
+ * Database optimization
+ * - improving database performance to reduce the execution time of a query
+ * 
+ * MySQL transaction
+ * - START TRANSACTION
+ * - BEGIN
+ * - COMMIT
+ * - ROLLBACK
+ * 
+ * Common Table Expression with multiple queries (CTE) 
+ * - how you can use CTE to manage
+ * 
+ * MySQL prepared statement
+ * - how to make use of prepared statement to limit the number of times MySQL must compile and parse code
+ * 
+ * MySQL JSON
+ * - how to interact with the MySQL database using the JSON
+ * 
+ * Data analysis/analytics
+ * - how to make use of data collected during data analysis by converting it into useful information that can inform future decisions
+ * 
+ * Different type of data analysis/analytics
+ * - DESCRIPTIVE
+ * - EXPLORATORY
+ * - INFERENTIAL
+ * - PREDICTIVE
+ * 
+ * - relationship between MySQL and data analysis including the benefits and limitation of MySQL as a data analytics tool
+ * 
+ * - how to perform data analysis in MySQL
+ * - JOIN
+ * - Subquery
+ * - VIEW
+ * 
+ * FULL OUTER JOIN
+ * - how to emulate a full outer join in MySQL to extract all records from two table including those that don't match
+*/
